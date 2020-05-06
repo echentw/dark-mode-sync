b204028cf876422d07ec70b547f439e08a16ecd1
Eric Chen <ericchen098@gmail.com>
2020-05-06 16:45:33 -0500

    update bundle script

--- bundle.sh
+++ bundle.sh
@@ -1,9 +1,13 @@
 rm -rf dist/
 mkdir dist/
 
+cp manifest.json dist/
+
 cp youtube.js dist/
 cp reddit.js dist/
-cp manifest.json dist/
+cp stackoverflow.js dist/
+cp twitter.js dist/
+cp twitch.js dist/
 
 cd dist/
 
